fc7f4519f589f15738142a7cda400a90de3377c5
alexandrzhulev <alexandrzhulev@gmail.com>
2015-03-27 00:18:55 +0200

    added viewing changes in edit modal dialog

--- Resources/public/js/jquery.ubertranslation.js
+++ Resources/public/js/jquery.ubertranslation.js
@@ -2,10 +2,17 @@
  * jQuery for UberTranslationAdminBundle
  */
 $(document).ready(function(){
+    var definedMessage = $("#translation_form_translation").val();
+    var definedLocale = $("#translation_form_locale").val();
+    var definedDomain = $("#translation_form_domain").val();
     $("#btn_edit_translation").click(function(){
-        $("#edited_translation").text(
-            $("#translation_form_translation").val()
-        );
+        //reset displaying changed properties
+        $("#changed_translation").css("display", "none");
+        $("#changed_locale").css("display", "none");
+        $("#changed_domain").css("display", "none");
+        $("#check_existence").css("display", "none");
+        $("#confirm_message").css("display", "none");
+
         var url = $("#path_for_check").attr('href');
         $.get(url, {
             key: $("#translation_form_key").val(),
@@ -13,10 +20,22 @@ $(document).ready(function(){
             domain: $("#translation_form_domain").val()
         }, function(data) {
             if (data.isExists == true) {
-                $("#check_existence").text("Exists");
+                $("#check_existence").css("display", "initial");
             } else {
-                $("#check_existence").text("Not Exists");
+                $("#confirm_message").css("display", "initial");
             }
         });
+        if ($("#translation_form_translation").val() != definedMessage) {
+            $("#edited_translation").text($("#translation_form_translation").val());
+            $("#changed_translation").css("display", "initial");
+        }
+        if ($("#translation_form_locale").val() != definedLocale) {
+            $("#edited_locale").text($("#translation_form_locale").val());
+            $("#changed_locale").css("display", "initial");
+        }
+        if ($("#translation_form_domain").val() != definedDomain) {
+            $("#edited_domain").text($("#translation_form_domain").val());
+            $("#changed_domain").css("display", "initial");
+        }
     });
 });
