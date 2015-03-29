b402a4337875cf175cd6de554b544d034ad4febc
NovikovViktor <viktor.novikov95@gmail.com>
2015-03-29 15:11:53 +0300

    trying to make js more good, try to bind check existense only on message key

--- Resources/public/js/jquery.ubertranslation.js
+++ Resources/public/js/jquery.ubertranslation.js
@@ -1,50 +1,56 @@
 /*
  * jQuery for UberTranslationAdminBundle
  */
-$(document).ready(function () {
-   // Wrap jQuery objects into vars to make sure, that we initialize all objects once
-    var $changed_translation = $("#changed_translation");
-    var $changed_locale = $("#changed_locale");
-    var $changed_domain = $("#changed_domain");
-    var $translation_form_locale = $("#translation_form_locale");
-    var $translation_form_domain = $("#translation_form_domain");
-    var $translation_form_translation = $("#translation_form_translation");
+(function ($) {
+    $(function () {
+        // Wrap jQuery objects into vars to make sure, that we initialize all objects once
+        var $changed_translation = $("#changed_translation");
+        var $changed_locale = $("#changed_locale");
+        var $changed_domain = $("#changed_domain");
+        var $translation_form_locale = $("#translation_form_locale");
+        var $translation_form_key = $("#translation_form_key");
+        var $translation_form_domain = $("#translation_form_domain");
+        var $translation_form_translation = $("#translation_form_translation");
 
-    var definedMessage = $translation_form_translation.val();
-    var definedLocale = $translation_form_locale.val();
-    var definedDomain = $translation_form_domain.val();
+        var definedMessage = $translation_form_translation.val();
+        var definedKey = $translation_form_key.val();
+        var definedLocale = $translation_form_locale.val();
+        var definedDomain = $translation_form_domain.val();
 
-    $("#btn_edit_translation").click(function () {
-        //reset displaying changed properties
-        $changed_translation.css("display", "none");
-        $changed_locale.css("display", "none");
-        $changed_domain.css("display", "none");
-        $("#check_existence").css("display", "none");
-        $("#confirm_message").css("display", "none");
+        $("#btn_edit_translation").click(function () {
+            //reset displaying changed properties
+            $changed_translation.css("display", "none");
+            $changed_locale.css("display", "none");
+            $changed_domain.css("display", "none");
 
-        var url = $("#path_for_check").attr('href');
-        $.get(url, {
-            key: $("#translation_form_key").val(),
-            locale: $translation_form_locale.val(),
-            domain: $translation_form_domain.val()
-        }, function (data) {
-            if (data.isExists == true) {
-                $("#check_existence").css("display", "initial");
-            } else {
-                $("#confirm_message").css("display", "initial");
+            $("#check_existence").hide();
+            $("#confirm_message").show();
+
+            if (definedKey != $translation_form_key.val()) {
+                console.log(definedKey);
+                console.log($translation_form_key.val());
+                var url = $("#path_for_check").attr('href');
+                $.get(url, {
+                    key: $translation_form_key.val(),
+                    locale: $translation_form_locale.val(),
+                    domain: $translation_form_domain.val()
+                }, function (data) {
+                    if (data.isExists == true) {
+                        $("#check_existence").show();
+                        $("#confirm_message").hide();
+                    }
+                });
+            }
+
+            if ($translation_form_translation.val() != definedMessage) {
+                $("#edited_translation").text($("#translation_form_translation").val());
+            }
+            if ($translation_form_locale.val() != definedLocale) {
+                $("#edited_locale").text($("#translation_form_locale").val());
+            }
+            if ($translation_form_domain.val() != definedDomain) {
+                $("#edited_domain").text($("#translation_form_domain").val());
             }
         });
-        if ($translation_form_translation.val() != definedMessage) {
-            $("#edited_translation").text($("#translation_form_translation").val());
-            $("#changed_translation").css("display", "initial");
-        }
-        if ($translation_form_locale.val() != definedLocale) {
-            $("#edited_locale").text($("#translation_form_locale").val());
-            $("#changed_locale").css("display", "initial");
-        }
-        if ($translation_form_domain.val() != definedDomain) {
-            $("#edited_domain").text($("#translation_form_domain").val());
-            $("#changed_domain").css("display", "initial");
-        }
     });
-});
+})(jQuery);
